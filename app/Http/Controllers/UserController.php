247a67389229ac7f49582dba826424dfb44801e8
Silfi Nazarina <silfinazarina@gmail.com>
2024-03-08 22:07:53 +0700

    praktikum2-1 done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,18 +8,16 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id'=> 2,
-            'username'=> 'manager_tiga',
-            'nama'=>'Manager 3',
-            'password'=>Hash::make('12345')
-        ];
-        UserModel::create($data);
+    public function index()
+    {
+        $user = UserModel::findOr(20, ['username','nama'], function(){
+            abort(404);
+        });
+        
+        return view('user', ['data' => $user]);
 
-        //coba akses model userModel
-        $user = UserModel::all();
-        return view('user', ['data'=>$user]);
+        //$user = UserModel::find(1);
+        //$user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstwhere('level_id', 1);
     }
 }

61bd42c974e1b6b6f4719e7bc14642214abc96e3
Silfi Nazarina <silfinazarina@gmail.com>
2024-04-04 10:20:45 +0700

    merapikan susunan file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,8 +19,10 @@ class UserController extends Controller
             'title'=> 'Daftar user yang terdaftar dalam sistem'
         ];
 
-        $activeMenu = 'user'; //set menu yang sedang aktif
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page'=>$page, 'activeMenu' => $activeMenu]);
+        $activeMenu = 'user';       //set menu yang sedang aktif
+        $level = LevelModel::all(); //ambil data level untuk filter level
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page'=>$page, 
+            'level'=> $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
@@ -28,6 +30,12 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
                     ->with('level');
+
+        //Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi

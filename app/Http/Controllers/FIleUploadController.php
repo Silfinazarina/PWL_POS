eb18421ef5dc4257b3c62c0e87fa92c85726e1fc
Silfi Nazarina <silfinazarina@gmail.com>
2024-05-12 23:11:55 +0700

    JS12-Prak.E done

--- app/Http/Controllers/FIleUploadController.php
+++ app/Http/Controllers/FIleUploadController.php
@@ -16,7 +16,14 @@ class FIleUploadController extends Controller
             'berkas'=>'required|file|image|max:500',]);  
             $textFile = $request->berkas->getClientOriginalName(); //mengambil extension file asal
             $namaFile = 'web-'.time().".".$textFile; //menyambung 3 string
-            $path = $request->berkas->storeAs('uploads', $namaFile);
+
+            $path = $request->berkas->move('gambar', $namaFile);
+            $path = str_replace("\\","//",$path);
+            echo "Variabel path berisi:$path <br>";
+
+            $pathBaru = asset('storage/'.$namaFile);
             echo "proses upload berhasil, data disimpan pada:$path";
+            echo "<br>";
+            echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
     }
 }

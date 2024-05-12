843819ba12cabe4d567d637a8854f40be6c7d5f9
Silfi Nazarina <silfinazarina@gmail.com>
2024-05-12 21:32:51 +0700

    JS12-Prak.B-C done

--- app/Http/Controllers/FIleUploadController.php
+++ app/Http/Controllers/FIleUploadController.php
@@ -11,6 +11,32 @@ class FIleUploadController extends Controller
     }
 
     public function prosesFileUpload(Request $request){  //memproses hasil submit form
-        return "Pemrosesan file upload di sini"; 
+
+        // validasi inputan form
+        $request->validate([
+            'berkas'=>'required|file|image|max:500',]);     //file:memastikan file berhasil diupload
+            echo $request->berkas->getClientOriginalName()."lolos validasi";
+
+        // dump($request->berkas);                          -->langsung men-dump sesuai name dari input
+        // return "Pemrosesan file upload di sini"; 
+
+        // informasi file upload
+        // if ($request->hasFile('berkas')) {
+        //     echo "path(): ".$request->berkas->path();      //menampilkan alamat path file
+        //     echo "<br>";
+        //     echo "extension(): ".$request->berkas->extension();  //menampilkan extension file
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): ".
+        //         $request->berkas->getClientOriginalExtension();  //menampilkan extension yang diambil dari nama file
+        //     echo "<br>";
+        //     echo "getMimeType(): ".$request->berkas->getMimeType(); //menampilkan mimetype dri file yg diupload
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): ". 
+        //     $request->berkas->getClientOriginalName();  //menampilkan nama asli dari file yang diupload
+        //     echo "<br>";
+        //     echo "getSize(): " .$request->berkas->getSize();  //menampilkan ukuran file yang di upload (dalam satuan byte)
+        // }else{
+        //     echo "Tidak ada berkas yang diupload";
+        // }
     }
 }
